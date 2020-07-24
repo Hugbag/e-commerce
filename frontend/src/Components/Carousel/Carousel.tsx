a551715931b25620739fe555cdee726c52ccba2a
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2020-07-24 21:13:17 +0200

    HUG-3 Add Carousel component and Home page

---
+++ frontend/src/Components/Carousel/Carousel.tsx
@@ -0,0 +1,30 @@
+// @ts-ignore
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    width: 930px;
+    margin: 0 auto;
+`;
+
+export default () => (
+    <StyledDiv>
+        <Carousel showThumbs={false} showStatus={false} infiniteLoop={true} autoPlay >
+                <div>
+                    <img alt="" src="https://hugbag.pl/uploads/slider/hugbag-2018-04-0926219mecek-slajder.jpg" />
+                </div>
+                <div>
+                    <img alt="" src="https://hugbag.pl/uploads/slider/hugbag-2018-10-16540882.jpg" />
+                </div>
+                <div>
+                    <img alt="" src="https://hugbag.pl/uploads/slider/hugbag-2020-04-3010717slajder_lubasha.jpg" />
+                </div>
+                <div>
+                    <img alt="" src="https://hugbag.pl/uploads/slider/hugbag-2020-04-3084514live_set.jpg" />
+                </div>
+            </Carousel>
+    </StyledDiv>
+
+);
